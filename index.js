97c4d87f791d4c6ab36ec4176ebdcd962b0504d5
islamix <27islamix@gmail.com>
2023-05-29 00:16:33 +0300

    .

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+ const rows = content.trim().split('\r\n').slice(1)
+ const data = rows.map((row) => row.split(','))
+
+ const averageAge = data.reduce((acc, passenger) => acc + Number(passenger[6]), 0) / data.length
+ 
+ const ports = data.reduce((acc, passenger) => {
+  const count = _.get(acc, passenger[12], 0) + 1
+  return { ...acc, [passenger[12]] : count}
+}, {})
+console.log(`Ports: ${JSON.stringify(ports)}`)
   // END
 }
